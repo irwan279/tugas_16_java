73567228e5ac46552a611bd58e00c62b062098fc
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 09:04:01 +0700

    tugas 16 java

---
+++ tugas16.js
@@ -0,0 +1,13 @@
+function minmax() {
+  var min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
+  console.log("NIlai Minimal :",min);
+  var max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
+  console.log("NIlai Maximal :",max);
+
+  console.log("=====kedua=====");
+  var min1 = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5)
+  console.log("NIlai Minimal :",min1);
+  var max1 = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5)
+  console.log("NIlai Maximal :",max1);
+}
+minmax();
